b14632259cfd5984641ef65bf64f537ef9caf9da
alexeyrusinov <ayrusinov@gmail.com>
2021-11-09 20:10:46 +0500

    test print pandas

--- base/__init__.py
+++ base/__init__.py
@@ -1,5 +1,6 @@
 import json, requests
 import datetime
+from pandas import DataFrame
 
 # get time
 now = datetime.datetime.now()
@@ -20,32 +21,49 @@ response = requests.get(url)
 response.raise_for_status()
 dic = response.json()
 
-#write json file
-with open('data.json', 'w', encoding='utf8') as f:
-    json.dump(dic, f, ensure_ascii=False, indent=4)
+df = DataFrame(dic['rasp'], columns =
+            ["number_route",
+            "name_route",
+            "time_otpr",
+            "time_prib",
+            "free_place",
+            "count_all_bus"
+            "count_bus",
+            "cancel",
+            "price",
+            "baggage",
+            "name_bus",
+            "name_atp",
+            "rasp_id"])
+print(df)
 
-#Read json file
-with open('data.json') as f:
-    data = json.load(f)
 
-# delete html code
-for item in data["rasp"]:
-    item["name_route"] = item["name_route"].replace('<br/>', '')
-
-# convert str ["time_otpr"] to int
-for item in data["rasp"]:
-    item["time_otpr"] = int("".join(filter(str.isdigit, item["time_otpr"])))
-
-#write json file
-with open('new_data.json', 'w', encoding='utf8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
-
-# print result
-for i in data["rasp"]:
-    if i["time_otpr"] < times:
-        continue
-    if i["cancel"] == "Отмена":
-        i["cancel"] = "canceled"
-    if i["cancel"] == "":
-        i["cancel"] = "waiting"
-    print(f"Time: {i['time_otpr']}, status: {i['cancel']}, free place: {i['free_place']}, name bus: {i['name_bus']}, {i['name_route']}")
+# #write json file
+# with open('data.json', 'w', encoding='utf8') as f:
+#     json.dump(dic, f, ensure_ascii=False, indent=4)
+#
+# #Read json file
+# with open('data.json') as f:
+#     data = json.load(f)
+#
+# # delete html code
+# for item in data["rasp"]:
+#     item["name_route"] = item["name_route"].replace('<br/>', '')
+#
+# # convert str ["time_otpr"] to int
+# for item in data["rasp"]:
+#     item["time_otpr"] = int("".join(filter(str.isdigit, item["time_otpr"])))
+#
+# #write json file
+# with open('new_data.json', 'w', encoding='utf8') as f:
+#     json.dump(data, f, ensure_ascii=False, indent=4)
+#
+# # print result
+# for i in data["rasp"]:
+#     if i["time_otpr"] < times:
+#         continue
+#     if i["cancel"] == "Отмена":
+#         i["cancel"] = "canceled"
+#     if i["cancel"] == "":
+#         i["cancel"] = "waiting"
+#     print(f"Time: {i['time_otpr']}, status: {i['cancel']}, free place: {i['free_place']}, name bus: {i['name_bus']}, {i['name_route']}")
