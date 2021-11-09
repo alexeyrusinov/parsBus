8853c719247d8c90db154e72b7debf05091636a5
alexeyrusinov <ayrusinov@gmail.com>
2021-11-09 19:38:01 +0500

    correct comments

--- base/__init__.py
+++ base/__init__.py
@@ -1,18 +1,18 @@
 import json, requests
 import datetime
 
-#получаю текущее вермя
+# get time
 now = datetime.datetime.now()
-# получаю текущю дату
+# get date
 now_day = str(now.day)
-# преобразую минуты и часы в строки и скоадываю
+# convert int (minutes and hours) to string and sum
 my_time = str(now.hour) + str(now.minute)
-# на выходе инт без нулей
+# на выходе инт
 times = int(my_time)
 
 
 
- # подставляю текущий день
+ # past now day
 url = "https://autovokzal.org/upload/php/result.php?id=1331&date=%272021-11-" + now_day  + "%27&station=ekb"
 
 
@@ -28,11 +28,11 @@ with open('data.json', 'w', encoding='utf8') as f:
 with open('data.json') as f:
     data = json.load(f)
 
-# убираю из строки html
+# delete html code
 for item in data["rasp"]:
     item["name_route"] = item["name_route"].replace('<br/>', '')
 
-# преобразую время отаправаления в число отбрасывая ноль
+# convert str ["time_otpr"] to int
 for item in data["rasp"]:
     item["time_otpr"] = int("".join(filter(str.isdigit, item["time_otpr"])))
 
@@ -40,7 +40,7 @@ for item in data["rasp"]:
 with open('new_data.json', 'w', encoding='utf8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
-
+# print result
 for i in data["rasp"]:
     if i["time_otpr"] < times:
         continue
