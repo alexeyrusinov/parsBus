e30c163db56ca7fea398f1292c7f8cb0497a89b6
alexeyrusinov <ayrusinov@gmail.com>
2021-11-10 19:15:52 +0500

    output nearest buses

--- base/__init__.py
+++ base/__init__.py
@@ -8,7 +8,7 @@ now = datetime.datetime.now()
 now_day = str(now.day)
 # convert int (minutes and hours) to string and sum
 my_time = str(now.hour) + str(now.minute)
-# output int
+# get time int
 times = int(my_time)
 
 
@@ -32,22 +32,23 @@ with open('data.json') as f:
     data = json.load(f)
 
 
-# delete html code and abbreviation of name
+items_to_keep = []
 for item in data["rasp"]:
-    item["name_route"] = item["name_route"].replace('г.Екатеринбург (Южный АВ) -<br/>', 'Екб (Южный АВ)')
-
-
-# convert str ["time_otpr"] to int
-for item in data["rasp"]:
+    # convert str ["time_otpr"] to int
     item["time_otpr"] = int("".join(filter(str.isdigit, item["time_otpr"])))
+    # добавляет только те автобусы у кого время отправления больше текущего времени
+    if item["time_otpr"] > times:
+        items_to_keep.append(item)
+    # delete html code and abbreviation of name
+    item["name_route"] = item["name_route"].replace('г.Екатеринбург (Южный АВ) -<br/>', 'Екб (Южный АВ)')
 
 
 #write json file
 with open('new_data.json', 'w', encoding='utf8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+    json.dump(items_to_keep, f, ensure_ascii=False, indent=4)
 
 
-df = DataFrame(data['rasp'], columns = ['time_otpr', 'cancel', 'free_place', 'name_bus', 'name_route'])
+df = DataFrame(items_to_keep, columns = ['time_otpr', 'cancel', 'free_place', 'name_bus', 'name_route'])
 # output result without index pandas
 print(df.to_string(index=False))
 
