de0f477d35a8362288350bc2d70c22cb9bcaf2ae
alexeyrusinov <ayrusinov@gmail.com>
2021-11-11 18:01:18 +0500

    past now_month in url

--- base/__init__.py
+++ base/__init__.py
@@ -5,14 +5,16 @@ from pandas import DataFrame
 now = datetime.datetime.now()
 # get date
 now_day = str(now.day)
+now_month = str(now.month)
 # convert int (minutes and hours) to string and sum
 my_time = str(now.hour) + str(now.minute)
 # get time int
 times = int(my_time)
 
 
-# past now day
-url = "https://autovokzal.org/upload/php/result.php?id=1331&date=%272021-11-" + now_day  + "%27&station=ekb"
+# past now day and month
+# url = "https://autovokzal.org/upload/php/result.php?id=1331&date=%272021-11-" + now_day  + "%27&station=ekb"
+url = "https://autovokzal.org/upload/php/result.php?id=1331&date=%272021-" + now_month + "-" + now_day + "%27&station=ekb"
 
 
 # get json
@@ -46,7 +48,7 @@ with open('new_data.json', 'w', encoding='utf8') as f:
     json.dump(items_to_keep, f, ensure_ascii=False, indent=4)
 
 
-# output of min to next bus
+# print of min to next bus
 for i in items_to_keep:
     if i["cancel"] == "":
         nex_bus = i["time_otpr"] - times
